c2ec9693fe3c54af502b4bea48084727bc0bba99
szilardinho <szilardinhog@gmail.com>
2017-04-05 16:59:26 -0400

    created subclass for material guidlines, set up tableView/cell, nav

--- wishListApp/MainViewController.swift
+++ wishListApp/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  wishListApp
 //
 //  Created by Szi Gabor on 4/4/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
