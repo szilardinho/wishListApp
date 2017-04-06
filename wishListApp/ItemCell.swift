b8c72f86623109c0c0886208c5b9c219b00cff3d
szilardinho <szilardinhog@gmail.com>
2017-04-06 15:05:48 -0400

    Table view and new item cell helper methods implemented

---
+++ wishListApp/ItemCell.swift
@@ -0,0 +1,20 @@
+//
+//  ItemCell.swift
+//  wishListApp
+//
+//  Created by Szi Gabor on 4/6/17.
+//  Copyright © 2017 Szi. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+    
+    
+}
--- wishListApp/MainViewController.swift
+++ wishListApp/MainViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,25 @@ class MainViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func tableView(_ tableView: UITableView, cellForRowAt IndexPath: IndexPath) -> UITableViewCell
+    {
+        return UITableViewCell()
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 0
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 0
+    }
+    
+    
 
 }
 
