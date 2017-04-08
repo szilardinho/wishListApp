9138aa24621e53cce62b1fbf70751bacff0fc81d
szilardinho <szilardinhog@gmail.com>
2017-04-08 17:00:06 -0400

    created details VC for add|edits

---
+++ wishListApp/ItemDetailsController.swift
@@ -0,0 +1,41 @@
+//
+//  ItemDetailsController.swift
+//  wishListApp
+//
+//  Created by Szi Gabor on 4/8/17.
+//  Copyright © 2017 Szi. All rights reserved.
+//
+
+import UIKit
+
+class ItemDetailsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let topItem = self.navigationController?.navigationBar.topItem {
+            
+            topItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        }
+        
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- wishListApp/MainViewController.swift
+++ wishListApp/MainViewController.swift
@@ -24,7 +24,7 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         tableView.delegate = self
         tableView.dataSource = self
         
-        generateTestData()
+       // generateTestData()
         attemptFetch()
         
 
